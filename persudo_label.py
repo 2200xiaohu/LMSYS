f55777901b8c25bb3a212c1e0895b33697562df8
root <295464699@qq.com>
2024-07-25 17:41:25 +0800

    persudo label

--- persudo_label.py
+++ persudo_label.py
@@ -70,6 +70,73 @@ seed_everything(42)
 
 from utils import load_split_data, load_json
 
+def adjust_values(A, B, a_space, b_space, ex_space):
+    # 计算A和a_space的差值
+    a_diff = a_space - A
+    b_diff = b_space - B
+    
+    # 第一种情况：A小于a_space，B小于b_space
+    if A < a_space and B < b_space:
+        ex_space += a_diff + b_diff
+        return A, B, ex_space
+
+    # 第二种情况：如果A和B都各自大于自己的space
+    elif A > a_space and B > b_space:
+        total_extra_needed = (A - a_space) + (B - b_space)
+        if total_extra_needed > ex_space:
+            A = int(a_space + ex_space / 2)
+            B = int(b_space + ex_space / 2)
+            ex_space = 0
+        else:
+            a_space = A
+            b_space = B
+            ex_space -= total_extra_needed
+            
+        return A, B, ex_space
+        
+    # 第三种情况：A或者B其中有一个大于a_space, b_space
+    elif A > a_space or B > b_space:
+        # 如果A大于a_space但是B小于b_space
+        if A > a_space and B < b_space:
+            extra_needed = A - a_space
+            ex_space += b_space - B
+            #够用
+            if ex_space >= extra_needed:
+                ex_space -= extra_needed
+                
+            else:
+                #不够用
+                #b_space = B + available_space
+                A = a_space + ex_space
+                ex_space = 0
+
+        # 如果B大于b_space但是A小于a_space
+        elif B > b_space and A < a_space:
+            extra_needed = B - b_space
+            ex_space += a_space - A
+            
+            if ex_space >= extra_needed:
+                ex_space -= extra_needed
+                
+            else:
+                B = b_space + ex_space
+                ex_space = 0
+
+        return A, B, ex_space
+    
+
+def adjust(current_lengths, prompt_length_space=300, response_length_space=800):
+    prompt_length = current_lengths[0]
+    response_a_length = current_lengths[1]
+    response_b_length = current_lengths[2]
+    #先看prompt的额度
+    ex_space = max(0, prompt_length_space - prompt_length)
+    response_a_length, response_b_length, ex_space = adjust_values(response_a_length, response_b_length, response_length_space, response_length_space, ex_space)
+    prompt_length = min(prompt_length, prompt_length_space)
+    prompt_length += ex_space
+
+    return prompt_length, response_a_length, response_b_length
+
 from torch.utils.data import Dataset
 class InstructionDataSet(Dataset):
     def __init__(self, data, tokenizer, max_source_length, max_target_length):
@@ -89,6 +156,9 @@ class InstructionDataSet(Dataset):
     def __getitem__(self, index):
         now_data = self.data.loc[index]
         idx = now_data['id']
+        
+        over_max_length = now_data['over_max_length']
+        
         templete_part1 = "<start_of_turn>user\nHere are two question-answering dialogues. Compare two model performance on answering question, determine which is better.\n\n"
         templete_part1_input_ids = self.tokenizer(text=templete_part1, add_special_tokens=True, padding=False)['input_ids']
         
@@ -97,12 +167,48 @@ class InstructionDataSet(Dataset):
         #print(f"templete_part2 is {templete_part2_input_ids}")
         templete_part3 = "<start_of_turn>model\n"
         templete_part3_input_ids = self.tokenizer(text=templete_part3, add_special_tokens=True, padding=False)['input_ids'][1:]
-        prompt_response = now_data['prompt_response']
-        #print(f"id is {now_data['id']}")
-        #print(prompt_response)
-        prompt_response_ids = self.tokenizer(text=prompt_response, add_special_tokens=True, truncation=True,
-                                          max_length=self.max_source_length, padding=False)['input_ids'][1:]
         
+        templete_part4_input_ids = self.tokenizer(text="\n\n", add_special_tokens=False, padding=False)['input_ids']
+        
+        if over_max_length:
+            prompt = "#Prompt\n" + now_data['overflow_prompt']
+            r_a = "#Response\n" + "##Model A\n" + now_data['overflow_response_a']
+            r_b = "##Model B\n" + now_data['overflow_response_b']
+            
+            prompt_ids = self.tokenizer(text=prompt, add_special_tokens=False, truncation=False, padding=False)['input_ids']
+            model_a_input_ids = self.tokenizer(text=r_a, add_special_tokens=False, truncation=False, padding=False)['input_ids']
+            model_b_input_ids = self.tokenizer(text=r_b, add_special_tokens=False, truncation=False, padding=False)['input_ids']
+
+            if len(prompt_ids) + len(model_a_input_ids) + len(model_b_input_ids) <= self.max_source_length:
+                prompt_response_ids = prompt_ids + model_a_input_ids + model_b_input_ids
+            
+            else:
+                '''
+                prompt 和 response 按照 300， 800， 800
+                response 优先
+                多的再给prompt
+                '''
+                length = [len(prompt_ids), len(model_a_input_ids), len(model_b_input_ids)]
+                print(f"before {len(prompt_ids) + len(model_a_input_ids) + len(model_b_input_ids)}")
+                print(f"before {length}")
+                prompt_max_length, a_max_length, b_max_length = adjust(length)
+                
+                prompt_ids = prompt_ids[:prompt_max_length] + templete_part4_input_ids
+                model_a_input_ids = model_a_input_ids[:a_max_length] + templete_part4_input_ids
+                model_b_input_ids = model_b_input_ids[:b_max_length] + templete_part4_input_ids
+                
+                prompt_response_ids = prompt_ids + model_a_input_ids + model_b_input_ids
+                print(f"after {[prompt_max_length, a_max_length, b_max_length]}")
+                print(f"after {len(prompt_response_ids)}")
+        
+        else:
+            prompt_response = now_data['prompt_response']
+            #print(f"id is {now_data['id']}")
+            #print(prompt_response)
+            prompt_response_ids = self.tokenizer(text=prompt_response, add_special_tokens=True, truncation=True,
+                                              max_length=self.max_source_length, padding=False)['input_ids'][1:]
+            #print(prompt_response_ids)        
+            
         input_ids = templete_part1_input_ids + prompt_response_ids + templete_part2_input_ids + templete_part3_input_ids
         input_text = self.tokenizer.decode(input_ids, skip_special_tokens=False)
         #print(f"input is {self.tokenizer.decode(input_ids)}")
@@ -127,27 +233,19 @@ def collate_fn(batch):
     return batch_input, batch['id']
 
 
-ex1 = pd.read_csv("dataset/kaggle-ultrafeedback-drop-duplicate.csv")
-ex2 = pd.read_csv("dataset/kaggle-ultrafeedback-ties-drop-duplicate.csv")
-
-ex1 = load_json(ex1)
-ex2 = load_json(ex2)
-
-data = pd.concat([ex1,ex2]).reset_index(drop = True)
-
-data['id'] = [i for i in range(len(data))]
-
-data.to_json("dataset/ex70k.json", index=False)
-
-data_path = "dataset/ex70k.json"
-prompt_type = 2
-MAX_INPUT = 2300
-if_train = True
-df_train , df_valid = load_split_data(data_path, prompt_type, MAX_INPUT, if_train, False, False)
+from utils import load_split_data
+data_path = "dataset/persudo_label/35k_in_1M_prediction_thr0_35k.json"
+prompt_type = 3
+MAX_INPUT = 1900
+if_train = False
+split = False
+if_drop_duplicate = True
+keep = 'last'
+df_train , df_valid = load_split_data(data_path, prompt_type, MAX_INPUT, if_train, split, False, if_drop_duplicate, 'last')
 test = df_train
-
 test['length'] = test['prompt_response'].apply(len)
 test = test.sort_values(by = ['length'], ascending = False).reset_index(drop = True)
+#test = test.loc[:10,:]
 
 from tqdm import tqdm
 def inference(model, test_dataloader):
@@ -158,20 +256,22 @@ def inference(model, test_dataloader):
             batch_input[k] = batch_input[k].to(device)
         with torch.no_grad():
             response = model.generate(**batch_input, max_new_tokens=1, return_dict_in_generate=True, output_scores=True)
-            #batch_input['input_ids'].shape[-1] + 1
             score = response.scores[0]
             A_prob, B_prob, C_prob = score[:,A_TOKEN_IDS], score[:,B_TOKEN_IDS], score[:,C_TOKEN_IDS]
-            logits = torch.cat([A_prob, B_prob, C_prob], dim=-1) / 1.1
-            #logits = torch.Tensor([[A_prob,B_prob,C_prob]]) / 1.1
+            #print(f"A_prob shape is {A_prob.shape}")
+            #logits = torch.Tensor([[A_prob,B_prob,C_prob]])
+            logits = torch.cat([A_prob, B_prob, C_prob], dim=-1)
             logits = torch.softmax(logits, dim=-1).cpu().numpy()
+            #print(f"logits is {logits.shape}")
             node_result = [[idx[i],logits[i]] for i in range(len(idx))]
-        test_predictions.extend(node_result)
+
+        test_predictions.append(node_result)
     return test_predictions
 
 device = torch.device("cuda:0")
 base_model = 'google/gemma-2-9b-it'
-model_path = "output/restful-spaceship-414/checkpoint-5000_896"
-MAX_LENGTH = 2300
+model_path = "output/misunderstood-flower-508/checkpoint-5459_8857"
+MAX_LENGTH = MAX_INPUT
 
 tokenizer = AutoTokenizer.from_pretrained(model_path, truncation_side = 'left')
 config = AutoConfig.from_pretrained(base_model, trust_remote_code=True)
@@ -199,7 +299,7 @@ A_TOKEN_IDS = tokenizer('A',add_special_tokens=True, truncation=True, max_length
 B_TOKEN_IDS = tokenizer('B',add_special_tokens=True, truncation=True, max_length=1024)['input_ids'][1:]
 C_TOKEN_IDS = tokenizer('C',add_special_tokens=True, truncation=True, max_length=1024)['input_ids'][1:]
 
-batch_size = 12
+batch_size = 4
 tokenized_dataset = InstructionDataSet(test, tokenizer, MAX_LENGTH, 1)
 
 test_dataloader = torch.utils.data.DataLoader(tokenized_dataset, batch_size = batch_size ,collate_fn=collate_fn)
@@ -207,11 +307,26 @@ test_dataloader = torch.utils.data.DataLoader(tokenized_dataset, batch_size = ba
 sub_pred = inference(model = model0, test_dataloader = test_dataloader)
 
 # 提取数据
-processed_data = []
-for item in sub_pred:
-    id = item[0].item()  # 获取id
-    array_values = item[1].tolist()  # 获取array并转换为列表
-    processed_data.append([id] + array_values)
+if batch_size != 1:
+    t = [j for i in sub_pred for j in i]
+    # 提取数据
+    processed_data = []
+    for item in t:
+        #item = item[0]
+        id = item[0]  # 获取id
+        array_values = item[1].tolist()  # 获取array并转换为列表
+        processed_data.append([id] + array_values)
+    
+
+else:
+    # 提取数据
+    processed_data = []
+    for item in sub_pred:
+        item = item[0]
+        id = item[0].item()  # 获取id
+        array_values = item[1].tolist()  # 获取array并转换为列表
+        processed_data.append([id] + array_values)
+
 new_columns = ['id', 'winner_model_a', 'winner_model_b', 'winner_tie']
 df = pd.DataFrame(processed_data, columns=new_columns)
 df = df.groupby('id').mean().reset_index()
